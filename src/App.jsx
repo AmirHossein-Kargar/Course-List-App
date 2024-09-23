22d396ecd114e08b4107c8b128e61ad74bb2adc7
Amir Hossein Kargar <amirkaargar@hotmail.com>
2024-09-23 08:09:06 +0330

    Component Development CourseList & CourseCard

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "/src/App.css";
+import CourseList from "./components/CourseList";
+
+function App() {
+  return (
+    <div>
+      <h1>My Courses (3)</h1>
+      <CourseList/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/CourseCard.jsx
@@ -0,0 +1,55 @@
+function CourseCard({ course }) {
+  return (
+    <div className="course-item">
+      <div className="course-item__img">
+        <img src={course.imageUrl} alt={course.title} />
+      </div>
+      <div className="course-item__detail">
+        <CourseCardBody
+          title={course.title}
+          description={course.description}
+          rate={course.rate}
+        />
+        <CourseCardFooter course={course} />
+      </div>
+    </div>
+  );
+}
+
+export default CourseCard;
+
+function CourseCardBody({title, description, rate}) {
+  return (
+    <div className="course-item__body">
+      <div>
+        <p className="title">{title}</p>
+        <div className="desc">{description}</div>
+      </div>
+      <span className="rate">{rate}</span>
+    </div>
+  );
+}
+
+function CourseCardFooter({course}) {
+  return (
+    <div className="course-item__footer">
+      <div className="tags">
+        {course.tags.map((t, index) => (
+          <span key={index} className="badge badge--secondary">
+            {course.tags}
+          </span>
+        ))}
+      </div>
+      <div className="caption">
+        <div className="date">
+          {new Date(course.start).toLocaleDateString("en-US", {
+            month: "short",
+            year: "numeric",
+            day: "numeric",
+          })}
+        </div>
+        <span className="badge badge--primary">{course.status}</span>
+      </div>
+    </div>
+  );
+}
